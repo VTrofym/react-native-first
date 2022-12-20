49025ae576fd37d6b1f98cf19e74798614fd9f43
VTrofym <v.trof@icloud.com>
2022-12-20 14:53:21 +0200

    ImageBackground

--- rn-social/App.js
+++ rn-social/App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+
+const image = { uri: "https://reactjs.org/logo-og.png" };
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={require("./assets/images/milky-way-2695569_1280.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>New app!</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 32,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });

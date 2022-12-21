27c782c15965f8c4cb730f6e05674a64e00ab2de
VTrofym <v.trof@icloud.com>
2022-12-21 12:38:18 +0200

    added LOGIN

--- rn-social/App.js
+++ rn-social/App.js
@@ -1,37 +1,167 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from "react-native";
 
-const image = { uri: "https://reactjs.org/logo-og.png" };
+const initialState = {
+  email: "",
+  password: "",
+};
 
 export default function App() {
+  console.log(Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setState] = useState(initialState);
+  const [dimensions, setDimensions] = useState(
+    Dimensions.get("window").width - 20 * 2
+  );
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 20 * 2;
+
+      setDimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log("state", state);
+    setState(initialState);
+  };
+
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("./assets/images/milky-way-2695569_1280.jpg")}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <Text style={styles.text}>New app!</Text>
-      </ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("./assets/images/milky-way-2695569_1280.jpg")}
+          style={styles.image}
+        >
+          <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 20 : 150,
+                width: dimensions,
+              }}
+            >
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>Hello!</Text>
+                <Text style={styles.headerTitle}>Welcome!</Text>
+              </View>
+              <View>
+                <Text style={styles.inputTitle}>EMAIL ADDRESS</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.email}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, email: value }))
+                  }
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+                <Text style={styles.inputTitle}>PASSWORD</Text>
+                <TextInput
+                  style={styles.input}
+                  textAlign={"center"}
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.password}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, password: value }))
+                  }
+                />
+              </View>
+              <TouchableOpacity
+                style={styles.btn}
+                activeOpacity={0.8}
+                onPress={keyboardHide}
+              >
+                <Text style={styles.btnTitle}>SIGN IN</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
   },
   image: {
     flex: 1,
-    justifyContent: "center",
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
-  text: {
-    color: "white",
-    fontSize: 32,
-    lineHeight: 84,
+  input: {
+    borderWidth: 2,
+    borderColor: "#fff8dc",
+    borderRadius: 5,
+    height: 40,
+    color: "#fff8dc",
+  },
+  form: {
+    // marginHorizontal: 40,
+  },
+  inputTitle: {
+    color: "#fff8dc",
+    marginBottom: 10,
+    fontSize: 18,
+  },
+  btn: {
+    height: 40,
+    borderRadius: 5,
+    borderWidth: 1,
+    marginTop: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 20,
+    ...Platform.select({
+      ios: {
+        borderColor: "#fff8dc",
+        backgroundColor: "transparent",
+      },
+      android: {
+        borderColor: "transparent",
+        backgroundColor: "#1e90ff",
+      },
+    }),
+  },
+  btnTitle: {
+    color: Platform.OS === "ios" ? "#1e90ff" : "#fff8dc",
+    fontSize: 18,
     fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000c0",
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 150,
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: "#fff8dc",
   },
 });
